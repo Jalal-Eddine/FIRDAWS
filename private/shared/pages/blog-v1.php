cfee73105168f149a9702c2951f86e5b021315c6
Jalal-Eddine El-Habbazi <Jalaleddine.elhabbazi@gmail.com>
2019-12-01 21:49:13 +0100

    update

--- private/shared/pages/blog-v1.php
+++ private/shared/pages/blog-v1.php
@@ -1,10 +1,10 @@
         
-    <section class="blog">
-            <div class="author">
-                    <img src="img/me-large.jpg" alt="Author" class="author__img">
-                    <div class="author__details">
+<section class="blog">
+    <div class="blog_header">
+                    <img src="img/me-large.jpg" alt="Author" class="blog_header_img">
+                    <div class="blog_header_details">
                         <h4 class="heading-4 heading-4--light">Jalal-Eddine</h4>
-                        <div class="author__details--contact">
+                        <div class="blog_header_details--contact">
                             <a href="https://twitter.com/HabbaziJalal" target="_blank">
                                 <ion-icon name="logo-twitter"></ion-icon>
                             </a>
@@ -18,24 +18,25 @@
                                 <ion-icon name="logo-github"></ion-icon>
                             </a>
                         </div>
-                        <p class="author__details--date">Published at - <?php echo date('D'); ?></p>
+                        <p class="blog_header_details--date">Published at - <?php echo date('D'); ?></p>
                     </div>
-            </div> 
-            <h1 class="blog__title">Blog Title</h1>
-            <h2 class="blog__title-2">Mini title for more information about the title </h2> 
-         <img class="blog__front" src="img/home.jpg" alt="hero" > 
+    </div> 
+    <div class="blog_content">
+        <h1 class="blog_content_title">Blog Title</h1>
+        <h2 class="blog_content_title-2">Mini title for more information about the title </h2> 
+        <img class="blog_content_front" src="img/home.jpg" alt="hero" > 
 
-        <p class="blog__paragraph">
+        <p class="blog_content_paragraph">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis, in magni! Enim repellendus esse odio ducimus distinctio placeat unde ratione dolorem ipsa soluta rerum magni, cupiditate nisi aliquid excepturi beatae.
         </p> 
 
-         <div class="blog__frame">
-            <h1 class="blog__frame--title">bloc TiTle</h1>
-            <p class="blog__paragraph">
+         <div class="blog_content_frame">
+            <h1 class="blog_content_title">bloc TiTle</h1>
+            <p class="blog_content_paragraph">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda ut aut necessitatibus. Voluptates magnam voluptatibus consequatur dolor aperiam dolorem voluptas ipsam corporis inventore aliquid labore corrupti ducimus, cumque eaque ab?
             </p>
             <img src="img/home.jpg" alt="hero">
         </div> 
 
-
-    </section> 
+    </div>
+</section> 
