900a8ca051fae2a658f94e9bd37380ad4784e3f6
Jalal-Eddine El-Habbazi <Jalaleddine.elhabbazi@gmail.com>
2019-12-02 14:23:48 +0100

    update

--- private/shared/public_header.php
+++ private/shared/public_header.php
@@ -69,14 +69,14 @@
         <header class="header">
             <img src="<?php echo url_for('/img/firdaws-2.png'); ?>" alt="Firdaws logo" class="header__logo">
             <h3 class="heading-3">Firdaws:</h3>
-            <h1 class="heading-1">The new way of education</h1>
+            <h1 class="heading-1">Sharing is Caring</h1>
             <button class="btn header__btn">View Our Courses</button>
             <div class="header__seenon-text">Seen on</div>
             <div class="header__seenon-logos">
-                <img src="<?php echo url_for('/img/logo-bbc.png'); ?>" alt="Seen on logo 1">
-                <img src="<?php echo url_for('img/logo-forbes.png'); ?>" alt="Seen on logo 2">
-                <img src="<?php echo url_for('img/logo-techcrunch.png'); ?>" alt="Seen on logo 3">
-                <img src="<?php echo url_for('img/logo-bi.png'); ?>" alt="Seen on logo 4">
+                <img src="<?php echo url_for('/img/icon-2.png'); ?>" alt="Seen on logo 1">
+                <img src="<?php echo url_for('img/icon.png'); ?>" alt="Seen on logo 2">
+                <img src="<?php echo url_for('img/icon-3.png'); ?>" alt="Seen on logo 3">
+                <img src="<?php echo url_for('img/icon-2.png'); ?>" alt="Seen on logo 4">
             </div>
         </header>
         <div class="about">
@@ -102,7 +102,7 @@
                                 <ion-icon name="logo-github"></ion-icon>
                             </a>
                         </div>
-                    <p class="about__sold">Student, Web Engineering</p>
+                    <p class="about__sold">Web Engineering, Student</p>
                 </div>
             </div>
         </div>
